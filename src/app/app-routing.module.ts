cff4da99c3bed94f5a8d1c6ee4ddb1c95217e87c
Formation <you@example.com>
2022-06-27 14:27:59 +0200

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './composants/accueil/accueil.component';
+import { AjouterunlivreComponent } from './composants/ajouterunlivre/ajouterunlivre.component';
 
-const routes: Routes = [];
+
+import { LivresComponent } from './composants/livres/livres.component';
+
+
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'livres', component: LivresComponent },
+  { path: 'ajouterunlivre', component: AjouterunlivreComponent },
+  
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LivresComponent } from './composants/livres/livres.component';
+import { AccueilComponent } from './composants/accueil/accueil.component';
+
+import { FormsModule } from '@angular/forms';
+import { AjouterunlivreComponent } from './composants/ajouterunlivre/ajouterunlivre.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+AjouterunlivreComponent,
+    LivresComponent,
+    AccueilComponent,
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
